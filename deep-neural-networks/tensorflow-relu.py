d7849e12246301ce2fb0dff69cf9d40bde536bda
Vinayak Kankanwadi <vinayakkankanwadi@hotmail.com>
2017-11-04 22:21:30 +1000

    add model and logit output

--- deep-neural-networks/tensorflow-relu.py
+++ deep-neural-networks/tensorflow-relu.py
@@ -23,6 +23,11 @@ biases = [
 features = tf.Variable([[1.0, 2.0, 3.0, 4.0], [-1.0, -2.0, -3.0, -4.0], [11.0, 12.0, 13.0, 14.0]])
 
 # TODO: Create Model
+hidden_layer = tf.add(tf.matmul(features, weights[0]), biases[0])
+hidden_layer = tf.nn.relu(hidden_layer)
+logits = tf.add(tf.matmul(hidden_layer, weights[1]), biases[1])
 
 # TODO: Print session results
-
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    print(sess.run(logits))
